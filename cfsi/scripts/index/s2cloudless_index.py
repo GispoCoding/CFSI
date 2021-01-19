649186840c8172f86f230a3785010d7de3da6580
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-19 13:49:50 +0200

    Add path conversion helper function
    Converts paths from container fs to host fs based on env variables.

--- cfsi/scripts/index/s2cloudless_index.py
+++ cfsi/scripts/index/s2cloudless_index.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import md5
 from logging import DEBUG
 from pathlib import Path
@@ -80,6 +81,22 @@ class S2CloudlessIndexer(ODCIndexer):
             }
         }
         return eo3
+
+    @staticmethod
+    def container_path_to_global_path(*file_paths: Path) -> List[Path]:
+        """ Translates container paths to global paths based on
+        CFSI_CONTAINER_OUTPUT and CFSI_OUTPUT_DIR env variables.
+        e.g. /output/tiles/... -> /home/ubuntu/cfsi_output/tiles/... """
+        res: List[Path] = []
+        container_output_path = os.environ["CFSI_CONTAINER_OUTPUT"]
+        external_output_path = os.environ["CFSI_OUTPUT_DIR"]
+        for file_path in file_paths:
+            if str(file_path).startswith(container_output_path):
+                res.append(Path(str(file_path).replace(container_output_path, external_output_path)))
+            else:
+                res.append(file_path)
+
+        return res
 
     def add_dataset(self, doc: Dict, **kwargs) -> (ODCDataset, Union[Exception, None]):
         """ Adds dataset to dcIndex """

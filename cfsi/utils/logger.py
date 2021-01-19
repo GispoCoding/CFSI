5412a06b01fcd3bb4e8480b12ca03737d4c967aa
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-19 12:37:05 +0200

    Remove rotating file logs
    Docker-compose already stores logs in a JSON file by default.
    The log files can also be rotated if necessary.

--- cfsi/utils/logger.py
+++ cfsi/utils/logger.py
@@ -1,15 +1,4 @@
-import os
 import logging
-from logging.handlers import RotatingFileHandler
-from pathlib import Path
-
-try:
-    LOGGING_DIR = Path(os.environ["CFSI_LOG_DIR"])
-except KeyError:
-    LOGGING_DIR = Path.home()
-
-if not LOGGING_DIR.exists():
-    LOGGING_DIR.mkdir(parents=True)
 
 
 def create_logger(
@@ -19,17 +8,11 @@ def create_logger(
      TODO: add rotating log file handler. """
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(level)
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
-
-    log_path = LOGGING_DIR.joinpath("cfsi.log")
-    file_handler = RotatingFileHandler(log_path, maxBytes=10000000, backupCount=5)
-    file_handler.setLevel(level)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
 
     return logger

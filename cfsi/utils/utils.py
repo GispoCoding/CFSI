2e25543817dc55624768b698f1037777cd1eb581
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-19 17:19:21 +0200

    Move s2 bucket name swap func to utils

--- cfsi/utils/utils.py
+++ cfsi/utils/utils.py
@@ -2,6 +2,9 @@ import os
 from typing import List
 from pathlib import Path
 
+L1C_BUCKET = "sentinel-s2-l1c"
+L2A_BUCKET = "sentinel-s2-l2a"
+
 
 def container_path_to_global_path(*file_paths: Path) -> List[Path]:
     """ Translates container paths to global paths based on
@@ -23,3 +26,12 @@ def container_path_to_global_path(*file_paths: Path) -> List[Path]:
             res.append(file_path)
 
     return res
+
+
+def swap_s2_bucket_names(uri: str) -> str:
+    """ Swaps L1C <-> L2A bucket names in given uri string """
+    if L1C_BUCKET in uri:
+        return uri.replace(L1C_BUCKET, L2A_BUCKET)
+    elif L2A_BUCKET in uri:
+        return uri.replace(L2A_BUCKET, L1C_BUCKET)
+    raise ValueError  # TODO: add custom exception

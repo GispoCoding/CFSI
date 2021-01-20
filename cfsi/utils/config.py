72fb83ca59be9563a444e6a81418ad08f01c6944
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-20 11:11:39 +0200

    Improve logging and error handling with configs

--- cfsi/utils/config.py
+++ cfsi/utils/config.py
@@ -6,6 +6,10 @@ from types import SimpleNamespace
 import yaml
 from yaml.parser import ParserError
 
+from cfsi.utils.logger import create_logger
+
+LOGGER = create_logger()
+
 
 def load_config() -> SimpleNamespace:
     """ Loads and returns CFSI config """
@@ -20,19 +24,32 @@ def load_config() -> SimpleNamespace:
         return SimpleNamespace(**d)
 
     try:
-        cfsi_base_dir = os.environ["CFSI_BASE_DIR"]
+        cfsi_base_dir = Path(os.environ["CFSI_BASE_DIR"])
+        if not cfsi_base_dir.exists():
+            LOGGER.critical("Directory specified in environment variable"
+                            "CFSI_BASE_DIR does not exist")
+            raise ValueError("Invalid CFSI_BASE_DIR value")  # TODO: custom exception
     except KeyError:
-        cfsi_base_dir = Path().cwd()
-    config_path = Path(cfsi_base_dir / "cfsi-config.yaml")
+        LOGGER.warning("Environment variable CFSI_BASE_DIR not set, "
+                       f"trying to load configuration from {Path().cwd()}")
+        cfsi_base_dir = Path().cwd()  # TODO: remove comment
+    config_path = Path(cfsi_base_dir / "cfsi_config.yaml")
     config_data = load_config_file(config_path)
     return dict_to_namespace(config_data)
 
 
 def load_config_file(config_path: Path):
     """ Loads and parses a configuration file from given path """
-    with open(config_path) as config_file:
-        try:
-            config_data = yaml.safe_load(config_file)
-        except ParserError:  # TODO: handle errors in cfg
-            raise
-        return config_data
+    try:
+        with open(config_path) as config_file:
+            try:
+                config_data = yaml.safe_load(config_file)
+            except ParserError:  # TODO: handle errors in cfg
+                raise
+            return config_data
+
+    except FileNotFoundError as err:
+        LOGGER.critical("Error loading configuration: " 
+                        "CFSI configuration file not found, " 
+                        f"ensure configuration file exists: {err}")
+        raise

5581b672cde515f4bac86f3e8718f2c26add41a6
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-16 18:24:19 +0200

    Add buffer for debugging

--- cfsi/utils/load_datasets.py
+++ cfsi/utils/load_datasets.py
@@ -6,6 +6,10 @@ from datacube.model import Dataset as ODCDataset
 import xarray as xa
 
 
+DEBUG_BUFFER = 0
+# DEBUG_BUFFER = 30000
+
+
 def dataset_from_odcdataset(
         product: str,
         datasets: Union[List[ODCDataset], ODCDataset] = None,
@@ -28,10 +32,29 @@ def dataset_from_odcdataset(
     if not isinstance(datasets, list):
         datasets = [datasets]
 
-    return dc.load(product=product,
+    product_name = datasets[0].metadata_doc["product"]["name"]
+    debug_ds = None
+    if DEBUG_BUFFER:
+        bounds = datasets[0].bounds
+        x = (bounds.left + DEBUG_BUFFER, bounds.right - DEBUG_BUFFER)
+        y = (bounds.top - DEBUG_BUFFER, bounds.bottom + DEBUG_BUFFER)
+        debug_ds = dc.load(product=product_name,
+                           dask_chunks={},
+                           measurements=measurements,
+                           output_crs="epsg:32635",
+                           resolution=(-10, 10),
+                           crs="epsg:32635",
+                           datasets=datasets,
+                           x=x, y=y)
+
+    real_ds = dc.load(product=product_name,
                    dask_chunks={},
                    measurements=measurements,
                    output_crs="epsg:32635",  # TODO: read from dataset
                    resolution=(-10, 10),  # TODO: read from dataset
                    crs="epsg:32635",  # TODO: read from dataset
                    datasets=datasets)
+
+    if DEBUG_BUFFER:
+        return debug_ds
+    return real_ds

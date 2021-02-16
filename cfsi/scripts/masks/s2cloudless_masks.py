784a1693685c5a3247db842b85ac8f8a46c69a48
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-16 17:51:23 +0200

    Move mask creation logic to S2CloudlessGenerator

--- cfsi/scripts/masks/s2cloudless_masks.py
+++ cfsi/scripts/masks/s2cloudless_masks.py
@@ -1,103 +1,174 @@
-from typing import Dict, Union
+from pathlib import Path
+from typing import Dict, List
 from logging import DEBUG
+from datacube import Datacube
 from datacube.model import Dataset as ODCDataset
 import numpy as np
 from s2cloudless import S2PixelCloudDetector
+from osgeo import gdal
 
+from cfsi import config
+from cfsi.scripts.index.s2cloudless_index import S2CloudlessIndexer
 from cfsi.utils.load_datasets import dataset_from_odcdataset
 from cfsi.utils.logger import create_logger
-from cfsi.utils.write_utils import generate_s2_file_output_path
-
-# TODO: define in a separate config file
-CLOUD_THRESHOLD = 0.3           # cloud threshold value for s2cloudless
-MAX_CLOUD_THRESHOLD = 100.0     # maximum cloudiness percentage in metadata
-MIN_CLOUD_THRESHOLD = 0.0       # minimum cloudiness percentage in metadata
-CLOUD_PROJECTION_DISTANCE = 30  # maximum distance to search for cloud shadows
-DARK_PIXEL_THRESHOLD = 0.15     # max band 8 value for pixel to be considered dark
-WRITE_RGB = False               # write L1C rgb for validating results
+from cfsi.utils.write_utils import generate_s2_file_output_path, odcdataset_to_tif, write_l1c_dataset
 
 LOGGER = create_logger("s2cloudless", level=DEBUG)
 
 
-def process_dataset(dataset: ODCDataset) -> (np.ndarray, np.ndarray):
-    """ Generate cloud and cloud shadow masks for a single datacube dataset """
-    tile_props = dataset.metadata_doc["properties"]
-    s3_key = tile_props["s3_key"]
-    metadata_cloud_percentage = cloud_percentage_inside_threshold(tile_props)
-    if isinstance(metadata_cloud_percentage, bool):
-        return
+class S2CloudlessGenerator:
 
-    LOGGER.info(f"Processing {s3_key}, {metadata_cloud_percentage}% cloudy")
-    ds = dataset_from_odcdataset("s2a_level1c_granule", dataset)
+    def __init__(self):
+        """ Constructor method """
+        self.max_iterations = config.masks.s2cloudless_masks.max_iterations
+        self.i = 1
+        self.indexed_masks: List[ODCDataset] = []
+        self.base_product_name = "s2a_level1c_granule"
+        self.mask_product_name = "s2a_level1c_s2cloudless"
 
-    LOGGER.info("Fetching data to array")
-    array = np.moveaxis(ds.to_array().values.astype("float64") / 10000, 0, -1)
-    LOGGER.info(f"Generating cloud masks for {s3_key}")
-    cloud_masks = generate_cloud_masks(array)
-    LOGGER.info(f"Generating shadow masks for {s3_key}")
-    shadow_masks = generate_cloud_shadow_masks(array[:, :, :, 7], cloud_masks, tile_props["mean_sun_azimuth"])
-    LOGGER.info("Mask generation done")
-    return cloud_masks, shadow_masks
+    def create_masks(self) -> List[ODCDataset]:
+        """ Creates masks based on config """
+        l1c_datasets = self.__get_l1c_datasets()
+        if len(l1c_datasets) < self.max_iterations or not self.max_iterations:
+            self.max_iterations = len(l1c_datasets)
 
+        for dataset in l1c_datasets:
+            if not self.__should_process(dataset):
+                continue
 
-def cloud_percentage_inside_threshold(tile_metadata: Dict) -> Union[float, bool]:
-    """ Checks if metadata cloud percentage of given dataset is within threshold """
-    metadata_cloud_percentage = tile_metadata["cloudy_pixel_percentage"]
-    if metadata_cloud_percentage > MAX_CLOUD_THRESHOLD:
-        LOGGER.info("Metadata cloud percentage greater than max threshold value: " +
-                    f"{MAX_CLOUD_THRESHOLD} < {metadata_cloud_percentage}")
-        return False
-    if metadata_cloud_percentage < MIN_CLOUD_THRESHOLD:
-        LOGGER.info("Metadata cloud percentage lower than min threshold value: " +
-                    f"{MIN_CLOUD_THRESHOLD} > {metadata_cloud_percentage}")
-        return False
-    return metadata_cloud_percentage
+            mask_arrays = self._process_dataset(dataset)
+            output_masks = self._write_mask_arrays(dataset, mask_arrays)
+            self.indexed_masks += S2CloudlessIndexer().index({dataset: output_masks})
 
+            if self.i > self.max_iterations:
+                LOGGER.warning(f"Reached maximum iterations count {self.max_iterations}")
+                break
+            self.i += 1
 
-def generate_cloud_masks(array: np.ndarray) -> np.ndarray:
-    """ Generate binary cloud masks with s2cloudless """
-    cloud_detector = S2PixelCloudDetector(threshold=CLOUD_THRESHOLD, all_bands=True)
-    return np.squeeze(cloud_detector.get_cloud_masks(array)).astype("byte")
+        if len(self.indexed_masks) == 0:
+            LOGGER.warning("No new masks generated")
+        return self.indexed_masks
 
+    def __get_l1c_datasets(self) -> List[ODCDataset]:
+        """ Gets all L1C datasets from ODC Index """
+        dc = Datacube(app="s2cloudless_mosaic")
+        l1c_datasets = dc.find_datasets(product=self.base_product_name)
+        return l1c_datasets
 
-def generate_cloud_shadow_masks(nir_array: np.ndarray,
-                                cloud_mask_array: np.ndarray,
-                                mean_sun_azimuth: float) -> np.ndarray:
-    """ Generate binary cloud shadow masks """
-    az = np.deg2rad(mean_sun_azimuth)
-    rows, cols = cloud_mask_array.shape
-    # calculate how many rows/cols to shift cloud shadow masks
-    x = np.math.cos(az)
-    y = np.math.sin(az)
-    x *= CLOUD_PROJECTION_DISTANCE
-    y *= CLOUD_PROJECTION_DISTANCE
-
-    new_rows = np.zeros((abs(int(y)), cols))
-    new_cols = np.zeros((rows, abs(int(x))))
-    # TODO: fix issues with projection direction
-    # DEBUG - Mean sun azimuth: 133.129531680158. Shifting shadow masks by 21 rows, -20 cols
-    # should shift towards top-left, i.e. -21 rows, -20 cols
-    LOGGER.debug(f"Mean sun azimuth: {mean_sun_azimuth}. " +
-                 f"Shifting shadow masks by {int(y)} rows, {int(x)} cols")
-    new_rows[:] = 1
-    new_cols[:] = 1
-
-    if y > 0:
-        shadow_mask_array = np.append(cloud_mask_array, new_rows, axis=0)[int(y):, :]
-    else:
-        shadow_mask_array = np.append(new_rows, cloud_mask_array, axis=0)[:int(y), :]
-    if x < 0:
-        shadow_mask_array = np.append(new_cols, shadow_mask_array, axis=1)[:, :int(x)]
-    else:
-        shadow_mask_array = np.append(shadow_mask_array, new_cols, axis=1)[:, int(x):]
-
-    dark_pixels = np.squeeze(np.where(nir_array <= DARK_PIXEL_THRESHOLD, 1, 0))
-    return np.where((cloud_mask_array == 0) & (shadow_mask_array == 1) & (dark_pixels == 1), 1, 0)
-
-
-def check_existing_masks(dataset: ODCDataset, product_name: str) -> bool:
-    """ Checks if a S2Cloudless mask for given dataset already exists """
-    output_directory = generate_s2_file_output_path(dataset, product_name).parent
-    if output_directory.exists():
+    def __should_process(self, dataset: ODCDataset) -> bool:
+        """ Checks if masks should be generated for given ODCDataset """
+        if self.__check_existing_masks(dataset):
+            LOGGER.info(f"S2Cloudless masks for dataset {dataset} already exist")
+            return False
+        metadata_cloud_percentage, in_threshold = self.__check_clouds_in_threshold(dataset)
+        if not in_threshold:
+            return False
         return True
-    return False
+
+    def __check_existing_masks(self, dataset: ODCDataset) -> bool:
+        """ Checks if a S2Cloudless mask for given dataset already exists """
+        # TODO: check if mask exists in index
+        output_directory = generate_s2_file_output_path(dataset, self.mask_product_name).parent
+        if output_directory.exists():
+            return True
+        return False
+
+    @staticmethod
+    def __check_clouds_in_threshold(dataset: ODCDataset) -> (float, bool):
+        """ Checks if metadata cloud percentage of given dataset is within threshold """
+        tile_metadata = dataset.metadata_doc["properties"]
+        cloud_percent = tile_metadata["cloudy_pixel_percentage"]
+        max_cloud = config.masks.s2cloudless_masks.max_cloud_threshold
+        min_cloud = config.masks.s2cloudless_masks.min_cloud_threshold
+        in_threshold = True
+
+        if cloud_percent > max_cloud:
+            LOGGER.info("Metadata cloud percentage greater than max threshold value: " +
+                        f"{max_cloud} < {cloud_percent}")
+            in_threshold = False
+
+        elif cloud_percent < min_cloud:
+            LOGGER.info("Metadata cloud percentage lower than min threshold value: " +
+                        f"{min_cloud} > {cloud_percent}")
+            in_threshold = False
+
+        return cloud_percent, in_threshold
+
+    def _process_dataset(self, dataset: ODCDataset) -> (np.ndarray, np.ndarray):
+        """ Generate cloud and cloud shadow masks for a single datacube dataset """
+        LOGGER.info(f"Iteration {self.i}/{self.max_iterations}: {dataset}")
+        tile_props = dataset.metadata_doc["properties"]
+        s3_key = tile_props["s3_key"]
+
+        ds = dataset_from_odcdataset("s2a_level1c_granule", dataset)
+
+        LOGGER.info("Fetching data to array")
+        array = np.moveaxis(ds.to_array().values.astype("float64") / 10000, 0, -1)
+        LOGGER.info(f"Generating cloud masks for {s3_key}")
+        cloud_masks = self.__generate_cloud_masks(array)
+        LOGGER.info(f"Generating shadow masks for {s3_key}")
+        shadow_masks = self.__generate_cloud_shadow_masks(
+            array[:, :, :, 7], cloud_masks, tile_props["mean_sun_azimuth"])
+        return cloud_masks, shadow_masks
+
+    @staticmethod
+    def __generate_cloud_masks(array: np.ndarray) -> np.ndarray:
+        """ Generate binary cloud masks with s2cloudless """
+        cloud_threshold = config.masks.s2cloudless_masks.cloud_threshold
+        cloud_detector = S2PixelCloudDetector(threshold=cloud_threshold, all_bands=True)
+        return np.squeeze(cloud_detector.get_cloud_masks(array)).astype("byte")
+
+    @staticmethod
+    def __generate_cloud_shadow_masks(nir_array: np.ndarray,
+                                      cloud_mask_array: np.ndarray,
+                                      mean_sun_azimuth: float) -> np.ndarray:
+        """ Generate binary cloud shadow masks """
+        az = np.deg2rad(mean_sun_azimuth)
+        rows, cols = cloud_mask_array.shape
+        # calculate how many rows/cols to shift cloud shadow masks
+        x = np.math.cos(az)
+        y = np.math.sin(az)
+        cloud_projection_distance = config.masks.s2cloudless_masks.cloud_projection_distance
+        x *= cloud_projection_distance
+        y *= cloud_projection_distance
+
+        new_rows = np.zeros((abs(int(y)), cols))
+        new_cols = np.zeros((rows, abs(int(x))))
+        # TODO: fix issues with projection direction
+        # DEBUG - Mean sun azimuth: 133.129531680158. Shifting shadow masks by 21 rows, -20 cols
+        # should shift towards top-left, i.e. -21 rows, -20 cols
+        LOGGER.debug(f"Mean sun azimuth: {mean_sun_azimuth}. " +
+                     f"Shifting shadow masks by {int(y)} rows, {int(x)} cols")
+        new_rows[:] = 2
+        new_cols[:] = 2
+
+        if y > 0:
+            shadow_mask_array = np.append(cloud_mask_array, new_rows, axis=0)[int(y):, :]
+        else:
+            shadow_mask_array = np.append(new_rows, cloud_mask_array, axis=0)[:-int(y), :]
+        if x > 0:
+            shadow_mask_array = np.append(new_cols, shadow_mask_array, axis=1)[:, :-int(x)]
+        else:
+            shadow_mask_array = np.append(shadow_mask_array, new_cols, axis=1)[:, -int(x):]
+
+        dark_pixel_threshold = config.masks.s2cloudless_masks.dark_pixel_threshold
+        dark_pixels = np.squeeze(np.where(nir_array <= dark_pixel_threshold, 1, 0))
+        return np.where((cloud_mask_array == 0) & (shadow_mask_array == 1) & (dark_pixels == 1), 1, 0)
+
+    @staticmethod
+    def _write_mask_arrays(dataset: ODCDataset,
+                           mask_arrays: (np.ndarray, np.ndarray)) -> Dict[str, Path]:
+        """ Writes cloud and shadow masks to files """
+        masks = {"clouds": mask_arrays[0],
+                 "shadows": mask_arrays[1]}
+        output_mask_files = odcdataset_to_tif(dataset, masks, "s2cloudless", gdal.GDT_Byte)
+        output_masks = {
+            "cloud_mask": output_mask_files[0],
+            "shadow_mask": output_mask_files[1]}
+
+        if config.masks.s2cloudless_masks.write_rgb:
+            write_l1c_dataset(dataset)
+        if config.masks.s2cloudless_masks.write_l1c:
+            write_l1c_dataset(dataset, rgb=False)
+
+        return output_masks
--- cfsi/scripts/process/s2cloudless_mosaic.py
+++ cfsi/scripts/process/s2cloudless_mosaic.py
@@ -1,20 +1,8 @@
-from pathlib import Path
-from typing import List, Dict
-
-import numpy as np
-from osgeo import gdal
-import datacube
-from datacube.model import Dataset as ODCDataset
-import datacube.storage._read  # TODO: Remove hack to avoid circular import ImportError
-
 from cfsi import config
 from cfsi.scripts.index.mosaic_index import MosaicIndexer
-from cfsi.scripts.masks.s2cloudless_masks import process_dataset
+from cfsi.scripts.masks.s2cloudless_masks import S2CloudlessGenerator
 from cfsi.utils.logger import create_logger
-from cfsi.scripts.index.s2cloudless_index import S2CloudlessIndexer
 from cfsi.scripts.mosaic.mosaic import MosaicCreator
-from cfsi.utils.write_utils import (generate_s2_file_output_path, odcdataset_to_tif,
-                                    write_l1c_dataset_rgb)
 
 
 LOGGER = create_logger("s2cloudless_mosaic")
@@ -22,46 +10,24 @@ WRITE_RGB = False
 
 
 def main():
-    """ Create s2cloudless masks for indexed L1C datasets """
-    LOGGER.info("Starting S2cloudless mosaic creator")
-    l1c_datasets = get_l1c_datasets()
+    """ Generate s2cloudless masks and create cloudless mosaics """
+    generate_masks()
+    create_mosaics()
+    exit(0)
 
-    max_iterations = config.masks.s2cloudless_masks.max_iterations
-    if len(l1c_datasets) < max_iterations or not max_iterations:
-        max_iterations = len(l1c_datasets)
 
-    # TODO: read products to generate masks for from config
-    i = 1
-    indexed_masks: List[ODCDataset] = []
-    for dataset in l1c_datasets:
-        if check_existing_masks(dataset, "s2a_level1c_s2cloudless"):
-            LOGGER.info(f"S2Cloudless masks for dataset {dataset} already exist")
-            continue
-
-        LOGGER.info(f"Iteration {i}/{max_iterations}: {dataset}")
-        mask_arrays = process_dataset(dataset)
-
-        LOGGER.info("Writing masks to file")
-        output_masks = write_mask_arrays(dataset, mask_arrays)
-
-        if config.masks.s2cloudless_masks.write_rgb:
-            write_l1c_dataset_rgb(dataset)
-
-        LOGGER.info(f"Finished writing {dataset}, indexing output")
-        indexed_masks += S2CloudlessIndexer().index({dataset: output_masks})
-
-        if max_iterations and i > max_iterations:
-            LOGGER.warning(f"Reached maximum iterations count {max_iterations}")
-            break
-        i += 1
-
-    if len(indexed_masks) == 0:
-        LOGGER.warning("No new masks generated")
-
+def generate_masks():
+    """ Generate s2cloudless masks """
     # TODO: read product names from config
+    S2CloudlessGenerator().create_masks()
+
+
+def create_mosaics():
+    """ Creates cloudless mosaics from s2cloudless masks """
     dates = config.mosaic.dates
     days = config.mosaic.range
     products = config.mosaic.products
+
     for product in products:
         for date_ in dates:
             mosaic_creator = MosaicCreator(product, date_, days)
@@ -69,43 +35,6 @@ def main():
             output_mosaic_path = mosaic_creator.write_mosaic_to_file(mosaic_ds)
             LOGGER.info("Indexing output mosaic")
             MosaicIndexer().index(mosaic_ds, output_mosaic_path)
-
-    exit(0)
-
-
-def get_l1c_datasets() -> List[ODCDataset]:
-    """ Gets all L1C datasets from ODC Index """
-    dc = datacube.Datacube(app="s2cloudless_mosaic")
-    l1c_datasets = dc.find_datasets(product="s2a_level1c_granule")
-    return l1c_datasets
-
-
-def check_existing_masks(dataset: ODCDataset, product_name: str) -> bool:
-    """ Checks if a S2Cloudless mask for given dataset already exists """
-    # TODO: check if mask exists in database instead of on disk
-    output_directory = generate_s2_file_output_path(dataset, product_name).parent
-    if output_directory.exists():
-        return True
-    return False
-
-
-def write_mask_arrays(dataset: ODCDataset,
-                      mask_arrays: (np.ndarray, np.ndarray)) -> Dict[str, Path]:
-    """ Writes cloud and shadow masks to files """
-    masks = {"clouds": mask_arrays[0],
-             "shadows": mask_arrays[1]}
-    output_mask_files = odcdataset_to_tif(dataset, masks, "s2cloudless", gdal.GDT_Byte)
-    output_masks = {
-        "cloud_mask": output_mask_files[0],
-        "shadow_mask": output_mask_files[1]}
-    return output_masks
-
-
-def get_mask_datasets() -> List[ODCDataset]:
-    """ Gets all S2Cloudless datasets from ODC Index """
-    dc = datacube.Datacube(app="s2cloudless_mosaic")
-    s2cloudless_datasets = dc.find_datasets(product="s2a_level1c_s2cloudless")
-    return s2cloudless_datasets
 
 
 if __name__ == "__main__":
